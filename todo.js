338f292593dc9cafabc403e109c06fb5dc09382a
bendtherules <abhasbhattacharya2@gmail.com>
2018-06-12 13:52:23 +0530

    Try adding TODO to master branch

---
+++ todo.js
@@ -0,0 +1,16 @@
+/**
+ * @todo Change babelrc browser support to support all modern browsers:
+
+```
+"browsers": [
+                        "> 1%",
+                        "last 2 versions",
+                        "not ie <= 8"
+                    ]
+```
+
+ */
+
+/** 
+* @todo In webpack, enable uglify
+*/ 
