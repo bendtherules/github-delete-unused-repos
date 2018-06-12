b5ab93cc82629db9dba21be9d3766c5132253b72
bendtherules <abhasbhattacharya2@gmail.com>
2018-06-12 13:35:45 +0530

    Initital working browser build

---
+++ todo.js
@@ -0,0 +1,11 @@
+// @TODO 1. Change babelrc browser support to :
+// ```
+// "browsers": [
+//                         "> 1%",
+//                         "last 2 versions",
+//                         "not ie <= 8"
+//                     ]
+// ```
+
+// @TODO 2: In webpack, enable uglify
+// 
